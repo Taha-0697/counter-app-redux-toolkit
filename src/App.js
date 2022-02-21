7a2b49c7d6357b7391b4338bcbc8837afb664bff
Taha <chaghtai5.tc@gmail.com>
2022-02-21 21:28:05 +0500

    redux counter app created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment ,reset } from "./redux/counter";
+import Counter from "./Component/Counter";
 
-const App = () => {
-    const { countValue } = useSelector((state) => state.counter);
-    const dispatch = useDispatch();
-    return (
-        <div style={{ textAlign: "center" }}>
-            <h1>The Count is : {countValue}</h1>
-            <button onClick={() => dispatch(increment()) }>Increment</button>
-            <button onClick={() => dispatch(decrement())}>Decrement</button>
-            <button onClick={() => dispatch(reset())}>Reset</button>
-        </div>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
 
 export default App;
---
+++ src/Component/Counter.js
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment ,reset } from "./../redux/counter";
+
+const Counter = () => {
+    const { countValue } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1>The Count is : {countValue}</h1>
+            <button onClick={() => dispatch(increment()) }>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <button onClick={() => dispatch(reset())}>Reset</button>
+        </div>
+    );
+};
+
+export default Counter;
